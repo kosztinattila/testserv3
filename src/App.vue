b9a8126289ae8ca54fa3430e73ee1fba39c15b6f
7ions <68548539+7ions@users.noreply.github.com>
2020-07-20 15:43:06 +0200

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="q-app">
+    <main-header></main-header>
+    <router-view />
+  </div>
+</template>
+
+<script lang="ts">
+  import header from "./components/header.vue";
+export default {
+  name: 'App',
+  components: {
+    "main-header": header,
+  }
+}
+</script>
---
+++ src/components/footer.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="main-body">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        function () {
+            return console.log("hello")
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
+
