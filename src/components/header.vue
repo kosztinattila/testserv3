d68895632ad97626ee23eedc995bba213a552b5b
7ions <kosztin@hotmail.com>
2020-08-08 23:05:38 +0200

    Changes to header.vue Adjusted responsiveness until 320px Implemented q-avatar on header logo Several CSS lines turned into Quasar classes (especially those, which structure the divs) The logo disappears at 365px upon screen resizing, which allows the header to align with smaller screen sizes
    Reaction to the review:
    -The Quasar Flexbox features are understood and used to separate elements in header
    -In20: The "label" section was simply removed, as it didnt serve any purposes
    -In82: .row is no longer used in CSS, all those settings are now running in Quasar classes
    -In55: all "float" properties has been removed from CSS and merged into Quasar classes
    -In16: all router-links have been swapped to @click event listeners
    -In6: The logo image is now nested in "q-avatar"

--- src/components/header.vue
+++ src/components/header.vue
@@ -1,124 +1,113 @@
 <template>
-    <div class="row main-row">
+  <div class="row main full-width justify-between ">
 
-        <div class="col q-pa-md">
-            <div class="row">
-                <div class="col container">
-                    <img class="logo" src="../assets/logo.png" alt="">
-                    <router-link to="/home"><h1>GOLDEN LEAVES</h1></router-link>
-                </div>
-            </div>
+    <div class="main-left q-pa-md">
+      <div class="row">
+        <div class="items-center row">
+          <q-avatar class="avatar" size="lg">
+            <img class="logo items-center" src="../assets/logo.png" alt="">
+          </q-avatar>
+          <h1 @click="$router.push('/home')" class="col">GOLDEN LEAVES</h1>
         </div>
-
-        <div class="col right-side q-pa-md">
-            <ul class="row">
-                <q-btn flat round dense icon="menu" class="col q-mr-sm" />
-                <li><router-link class="col" to="/buying" exact><q-btn style="background: goldenrod; color: white" label="Buy now"></q-btn></router-link></li>
-                <li><router-link class="col" to="/selling" exact><q-btn style="background: goldenrod; color: white" label="Sell"></q-btn></router-link></li>
-            </ul>
-            <form>
-                <label class="label">
-                    <q-input class="search-bar row" dark borderless value="">
-                        <template v-slot:append>
-                            <q-icon name="search" />
-                        </template>
-                    </q-input>
-                </label>
-            </form>
-        </div>
-
+      </div>
     </div>
+
+    <div class="row q-pa-sm wrap items-center">
+      <ul class="row">
+        <li>
+          <q-btn @click="$router.push('/buying')" style="overflow: auto; background: goldenrod; color: white"
+                 label="Buy now"></q-btn>
+        </li>
+        <li>
+          <q-btn @click="$router.push('/selling')" style="overflow: auto; background: goldenrod; color: white"
+                 label="Sell"></q-btn>
+        </li>
+      </ul>
+      <form class="row">
+        <q-input class="search-bar items-center fit" dark borderless value="">
+          <template v-slot:append>
+            <q-icon name="search"/>
+          </template>
+        </q-input>
+      </form>
+      <q-btn flat round dense icon="menu" class="q-mr-sm"/>
+    </div>
+
+  </div>
 </template>
 
 <script>
-    export default {
-        data: () => {
-            return {
-                navigators: [],
-                buttons: [],
-                search: ''
-            }
-        }
+export default {
+  data: () => {
+    return {
+      navigators: [],
+      buttons: [],
+      search: ''
     }
+  }
+}
 </script>
 
 <style scoped>
-    .main-row {
-        font-family: 'Lato', sans-serif;
-        font-size: 20px;
-        color: white;
-        background-color: #262626;
-        display: flex;
-        justify-content: space-between;
-        min-width: 360px;
-    }
-    .right-side {
-        display: flex;
-        float: right;
-    }
-    ul {
-        list-style-type: none;
-        padding: 0;
-        margin: 0;
-        text-decoration: none;
-    }
-    li {
-        margin-right: 1rem;
-    }
-    li:hover, :visited, :link, :active {
-        text-decoration: none;
-        color: white;
-    }
-    .container {
-        align-items: center;
-        display: flex;
+.main {
+  font-family: 'Lato', sans-serif;
+  font-size: 20px;
+  color: white;
+  background-color: #262626;
+  min-width: 320px;
+}
 
-    }
-    .q-pa-md {
-        margin-left: 1rem;
-        padding: 0;
+ul {
+  list-style-type: none;
+  margin: 0;
+  padding: 0;
+}
 
-    }
-    .row {
-        display: flex;
-        align-items: center;
+li {
+  margin-right: 1rem;
+}
 
-    }
-    h1 {
-        padding-left: .5rem;
-        font-size: 30px;
-        margin: 0;
-        white-space: nowrap;
-        display: flex;
-    }
-    .logo {
-        height: 2rem;
-        width: 2rem;
-    }
-    form {
-        display: flex;
-    }
-    @media only screen and (max-width: 800px) {
-        li, form {
-            display: none;
-        }
-    }
-    @media only screen and (min-width: 800px) {
-        .q-mr-sm {
-            display: none;
-        }
-    }
-    @media (min-width: 0) {
-        .col {
-            flex: none
-        }
-    }
-    .search-bar {
-      height: 2.572em;
-      border-radius: 0.2em;
-      padding-left: 1em;
-      margin-right: 2em;
-      width: 13em;
-      text-transform: uppercase;
-    }
+li:hover, :visited, :link, :active {
+  text-decoration: none;
+  color: white;
+}
+
+.main-left {
+  margin-left: 1rem;
+  padding: 0;
+
+}
+
+h1 {
+  font-size: 30px;
+  margin: 0;
+}
+
+@media only screen and (max-width: 800px) {
+  li, form {
+    display: none;
+  }
+}
+
+@media only screen and (min-width: 800px) {
+  .q-mr-sm {
+    display: none;
+  }
+}
+
+@media only screen and (max-width: 365px) {
+  .avatar {
+    display: none;
+  }
+}
+
+.search-bar {
+  padding-left: 1em;
+  margin-right: 1em;
+  text-transform: uppercase;
+}
+
+.avatar {
+  margin-right: 0.5rem;
+}
 </style>
