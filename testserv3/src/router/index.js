2cb014203f481a50761d27a8f9b95d00613764b4
7ions <kosztin@hotmail.com>
2020-08-10 02:40:12 +0200

    Comitting unversioned files

--- testserv3/src/router/index.js
+++ testserv3/src/router/index.js
@@ -16,13 +16,13 @@ Vue.use(VueRouter)
 
 export default function (/* { store, ssrContext } */) {
   return new VueRouter({
-                         scrollBehavior: () => ({x: 0, y: 0}),
-                         routes,
+    scrollBehavior: () => ({ x: 0, y: 0 }),
+    routes,
 
-                         // Leave these as they are and change in quasar.conf.js instead!
-                         // quasar.conf.js -> build -> vueRouterMode
-                         // quasar.conf.js -> build -> publicPath
-                         mode: process.env.VUE_ROUTER_MODE,
-                         base: process.env.VUE_ROUTER_BASE
-                       })
+    // Leave these as they are and change in quasar.conf.js instead!
+    // quasar.conf.js -> build -> vueRouterMode
+    // quasar.conf.js -> build -> publicPath
+    mode: process.env.VUE_ROUTER_MODE,
+    base: process.env.VUE_ROUTER_BASE
+  })
 }
