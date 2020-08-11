1f4481d41d83c5c738fc9637712811e8d3fecc73
7ions <kosztin@hotmail.com>
2020-08-11 12:17:49 +0200

    Changes to the whole project (version 2) - serverConnection.js has been added to .gitignore, as it contains secret data - firebase and vuex files has been added

---
+++ testserv3/src/store/store.js
@@ -0,0 +1,22 @@
+import { firebaseAuth, firebaseDb } from "boot/serverConnection";
+
+const state = {
+
+}
+const mutations = {
+
+}
+const actions = {
+
+}
+const getters = {
+
+}
+
+export default {
+    namespace: true,
+    state,
+    mutations,
+    actions,
+    getters,
+}
